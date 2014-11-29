6d5557caaf05ababc1a083cec02c368ff5f10c83
Moez Bouhlel <bmoez.j@gmail.com>
2014-11-29 16:19:50 +0100

    make mpd/ncmpcpp the default music player

--- ~/.i3/TERM.sh
+++ ~/.i3/TERM.sh
@@ -2,12 +2,12 @@ urxvtc -e zsh -c "tmux kill-session -t TERM; \
 exec tmux new-session -nTERM -sTERM \; \
 new-window -n TERM htop             \; \
 split -h -l 198 -t 0 zsh            \; \
-split -h -l 105  -t 1 mutt          \; \
-split -h -l 21  -t 2 cmus           \; \
+split -h -l 90  -t 1 mutt           \; \
 split -v -l 79  -t 0 newsbeuter     \; \
-split -v -l 15  -t 4 calcurse       \; \
+split -v -l 15  -t 3 calcurse       \; \
 split -v -l 65  -t 2 weechat        \; \
-split -v -l 65  -t 3 lyvi           \; \
+split -v -l 25  -t 5 ncmpcpp        \; \
+split -h -l 35  -t 6 lyvi           \; \
 select-pane -t 1
 "
 
