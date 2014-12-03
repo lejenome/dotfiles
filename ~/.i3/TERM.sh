1ebdbc487ddb106c693860bf0aa61290456b0787
Moez Bouhlel <bmoez.j@gmail.com>
2014-12-03 20:40:56 +0100

    Make sure my env help me focus on my work

--- ~/.i3/TERM.sh
+++ ~/.i3/TERM.sh
@@ -2,12 +2,10 @@ urxvtc -e zsh -c "tmux kill-session -t TERM; \
 exec tmux new-session -nTERM -sTERM \; \
 new-window -n TERM htop             \; \
 split -h -l 198 -t 0 zsh            \; \
-split -h -l 90  -t 1 mutt           \; \
+split -h -l 81  -t 1 mutt           \; \
 split -v -l 79  -t 0 newsbeuter     \; \
-split -v -l 15  -t 3 calcurse       \; \
+split -v -l 30  -t 3 calcurse       \; \
 split -v -l 65  -t 2 weechat        \; \
 split -v -l 25  -t 5 ncmpcpp        \; \
-split -h -l 35  -t 6 lyvi           \; \
 select-pane -t 1
 "
-
