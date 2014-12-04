cb81f1a693d4e91fe4a862e9bfd45e74031d9819
Moez Bouhlel <bmoez.j@gmail.com>
2014-12-04 16:52:20 +0100

    [part 2] improve conky

--- ~/.i3/TERM.sh
+++ ~/.i3/TERM.sh
@@ -1,11 +1,8 @@
 urxvtc -e zsh -c "tmux kill-session -t TERM; \
 exec tmux new-session -nTERM -sTERM \; \
-new-window -n TERM htop             \; \
-split -h -l 198 -t 0 zsh            \; \
-split -h -l 81  -t 1 mutt           \; \
-split -v -l 79  -t 0 newsbeuter     \; \
-split -v -l 30  -t 3 calcurse       \; \
-split -v -l 65  -t 2 weechat        \; \
-split -v -l 25  -t 5 ncmpcpp        \; \
-select-pane -t 1
+new-window -n TERM zsh              \; \
+split -h -l 81  -t 0 mutt           \; \
+split -v -l 65  -t 1 weechat        \; \
+split -v -l 25  -t 2 ncmpcpp        \; \
+select-pane -t 0
 "
