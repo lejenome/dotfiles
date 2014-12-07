a99254ba6520221db6abb545c4b190915ea88359
Moez Bouhlel <bmoez.j@gmail.com>
2014-12-07 23:34:14 +0000

    update MAIN.sh script

--- ~/.i3/MAIN.sh
+++ ~/.i3/MAIN.sh
@@ -1,1 +1,4 @@
-tmux new-session -t $USER -s `date +%Y%m%d%H%M%S` \; new-window
+tmux new-session -t $USER -s `date +%Y%m%d%H%M%S` \; new-window \
+	|| tmux has-session -t $USER \
+	|| tmux new-session -s $USER -d \
+	&& tmux new-session -t $USER \; new-window
