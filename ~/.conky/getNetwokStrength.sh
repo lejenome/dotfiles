1575637752dea19dac6055d3a0e98a90fe19484f
Moez Bouhlel <bmoez.j@gmail.com>
2014-12-13 01:54:49 +0000

    make systemd-networkd the default network manager

--- ~/.conky/getNetwokStrength.sh
+++ ~/.conky/getNetwokStrength.sh
@@ -1,2 +1,4 @@
 #dbus-send --system --print-reply --dest=org.ofono $(dbus-send --system --print-reply --dest=org.ofono / org.ofono.Manager.GetModems | grep "object path" | sed 's/.*"\(.*\)"/\1/') org.ofono.NetworkRegistration.GetProperties | grep "byte.*" -o | awk '{print $2}'
-connmanctl services $(connmanctl services | grep "^*" -m 1 | sed 's/^.* //') | awk  '$1 == "Strength" { print $3 }'
+echo 100 $(iw dev wlp8s0 station dump | grep signal: | sed 's/^.*\[\(.*\)].*$/\1/' ) | bc \
+	|| connmanctl services $(connmanctl services | grep "^*" -m 1 | sed 's/^.* //') | awk  '$1 == "Strength" { print $3 }' \
+	|| echo 0
