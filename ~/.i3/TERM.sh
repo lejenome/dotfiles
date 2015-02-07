43343dfd2523d0f282e9458468ab39224f0c6fbb
Moez Bouhlel <bmoez.j@gmail.com>
2015-02-07 19:33:36 +0000

    change font

--- ~/.i3/TERM.sh
+++ ~/.i3/TERM.sh
@@ -2,7 +2,7 @@ urxvtc -e zsh -c "tmux kill-session -t TERM; \
 exec tmux new-session -nTERM -sTERM \; \
 new-window -n TERM zsh              \; \
 split -h -l 81  -t 0 mutt           \; \
-split -v -l 65  -t 1 weechat        \; \
-split -v -l 25  -t 2 ncmpcpp        \; \
+split -v -l 50  -t 1 weechat        \; \
+split -v -l 15  -t 2 ncmpcpp        \; \
 select-pane -t 0
 "
