986da8dbac1437f5b3cc126921084ae097ef0ea4
Moez Bouhlel <bmoez.j@gmail.com>
2015-02-08 21:24:32 +0000

    undo zsh st fix

--- ~/.zsh/08-line-editor.zsh
+++ ~/.zsh/08-line-editor.zsh
@@ -1,5 +1,5 @@
-# fix for st: http://git.suckless.org/st/tree/FAQ
-function zle-line-init () { echoti smkx }
-function zle-line-finish () { echoti rmkx }
-zle -N zle-line-init
-zle -N zle-line-finish
+## fix for st: http://git.suckless.org/st/tree/FAQ
+#function zle-line-init () { echoti smkx }
+#function zle-line-finish () { echoti rmkx }
+#zle -N zle-line-init
+#zle -N zle-line-finish
