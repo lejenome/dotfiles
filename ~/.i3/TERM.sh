060ff26f10f7ab050e819d8e34c64e557bbc25cb
Moez Bouhlel <bmoez.j@gmail.com>
2015-02-08 20:56:32 +0000

    default to st instead of urxvt

--- ~/.i3/TERM.sh
+++ ~/.i3/TERM.sh
@@ -1,4 +1,4 @@
-urxvtc -e zsh -c "tmux kill-session -t TERM; \
+st -e zsh -c "tmux kill-session -t TERM; \
 exec tmux new-session -nTERM -sTERM \; \
 new-window -n TERM zsh              \; \
 split -h -l 81  -t 0 mutt           \; \
