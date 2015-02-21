ca793fecc664f0b73a7f11de026f461d2255d78b
Moez Bouhlel <bmoez.j@gmail.com>
2015-02-21 16:45:02 +0100

    update i3 config & zsh zstyle

--- ~/.zsh/07-zstyle.zsh
+++ ~/.zsh/07-zstyle.zsh
@@ -17,12 +17,12 @@ zstyle ':completion:*:*:kill:*:processes' list-colors "=(#b) #([0-9]#)*=36=31"
 
 ## VCS
 # vcs_info
-zstyle ':vcs_info:*'                      enable git hg svn
+zstyle ':vcs_info:*'                      enable git hg svn bzr
 # check-for-changes can be really slow.
 # you should disable it, if you work with large repositories   
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' unstagedstr '%F{62}D%F{237}IRTY%f'  # display ¹ if there are unstaged changes
-zstyle ':vcs_info:*' stagedstr '%F{62}S%F{237}TAGED'    # display ² if there are staged changes
+#zstyle ':vcs_info:*' check-for-changes true
+#zstyle ':vcs_info:*' unstagedstr '%F{62}D%F{237}IRTY%f'  # display ¹ if there are unstaged changes
+#zstyle ':vcs_info:*' stagedstr '%F{62}S%F{237}TAGED'    # display ² if there are staged changes
 zstyle ':vcs_info:*' actionformats "${FMT_BRANCH}${FMT_ACTION}" "${FMT_PATH}"
 zstyle ':vcs_info:*' formats       "${FMT_BRANCH}"              "${FMT_PATH}"
 zstyle ':vcs_info:*' nvcsformats   ""                           "%~"
@@ -34,7 +34,7 @@ zstyle ':completion:*'                       list-dirs-first true
 zstyle ':completion:*:default'               list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*'                       menu select=200
 zstyle ':completion:*'                       use-perl=1
-zstyle ':completion:*'                       my-accounts='m@japh.se'
+zstyle ':completion:*'                       my-accounts="$EMAIL"
 
 zstyle ':completion:*'                       squeeze-slashes true
 zstyle ':completion:*:cd:*'                  ignore-parents parent pwd
@@ -45,24 +45,24 @@ zstyle ':completion:*:*:kill:*:processes' \
   list-colors '=(#b) #([0-9]#)*=0=01;31'
 
 zstyle ':completion::complete:*'             use-cache on
-zstyle ':completion::complete:*'             cache-path ~/etc/cache/$HOST
+zstyle ':completion::complete:*'             cache-path ~/.zcompcache
 zstyle ':completion:*:processes'             command 'ps -axw'
 zstyle ':completion:*:processes-names'       command 'ps -awxho command'
 zstyle ':completion:*'                       matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*:functions'             ignored-patterns '_*'
 
 zstyle ':completion:*' group-name            ''
-zstyle ':completion:*:*:mplayer:*'           tag-order files
-zstyle ':completion:*:*:mplayer:*'           file-patterns   \
+zstyle ':completion:*:*:(mplayer|mpv):*'           tag-order files
+zstyle ':completion:*:*:(mplayer|mpv):*'           file-patterns   \
        '*.(rmvb|mkv|mpg|wmv|mpeg|avi|flv|mp3|mp4|flac|ogg):video' \
        '*:all-files' '*(-/):directories'
 
-zstyle ':completion:*:*:(vim|rview|vimdiff|xxd):*:*files' \
-  ignored-patterns '*~|*.(old|bak|zwc|viminfo|rxvt-*|zcompdump)|pm_to_blib|cover_db|blib' \
+zstyle ':completion:*:*:(vim|rview|vimdiff|xxd|nvim):*:*files' \
+  ignored-patterns '*~|*.(old|bak|zwc|viminfo|rxvt-*|zcompdump|zcompcache)|pm_to_blib|cover_db|blib' \
   file-sort modification
-zstyle ':completion:*:*:(vim|rview|vimdiff|xxd):*' \
+zstyle ':completion:*:*:(vim|rview|vimdiff|xxd|nvim):*' \
   file-sort modification
-zstyle ':completion:*:*:(vim|rview|vimdiff|xxd):*' \
+zstyle ':completion:*:*:(vim|rview|vimdiff|xxd|nvim):*' \
   tag-order files
 #zstyle ':completion:*:vim:*:directories' ignored-patterns \*
 
@@ -82,8 +82,7 @@ zstyle ':completion:*:descriptions' \
 zstyle ':completion:*:warnings' \
   format $'%{No match for \e[38;5;240;1m%}%d%{\e[m%}'
 
-zstyle ':completion:*:*:apvlv:*'             tag-order files
-zstyle ':completion:*:*:apvlv:*'             file-patterns '*.pdf'
+zstyle ':completion:*:*:(apvlv|zathura):*'             tag-order files
 
 zstyle ':completion:most-accessed-file:*' match-original both
 zstyle ':completion:most-accessed-file:*' file-sort access
@@ -112,9 +111,6 @@ zstyle ':completion:*:(ssh|scp):*:hosts-ipaddr' ignored-patterns \
 
 zstyle ':completion:*:(ssh|scp):*:users' ignored-patterns \
       adm bin daemon halt lp named shutdown sync
-
-zstyle ':completion:*:(ssh|scp):*:my-accounts' users-hosts \
-  'crshd@shellmix.com' 'mobile@192.168.0.112' 'mobile@192.168.1.49'
 
 
 zstyle ':completion:*:*:*:users' ignored-patterns \
