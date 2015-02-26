67e136399d6d0684e88bf024127aafaa988b4774
Moez Bouhlel <bmoez.j@gmail.com>
2015-02-26 11:23:13 +0100

    update TERM.sh script

--- ~/.i3/TERM.sh
+++ ~/.i3/TERM.sh
@@ -1,8 +1,7 @@
-st -e zsh -c "tmux kill-session -t TERM; \
-exec tmux new-session -nTERM -sTERM \; \
-new-window -n TERM zsh              \; \
-split -h -l 81  -t 0 mutt           \; \
-split -v -l 50  -t 1 weechat        \; \
-split -v -l 15  -t 2 ncmpcpp        \; \
+st -e zsh -c "tmux kill-session -t TERM;	\
+exec tmux new-session -nTERM -sTERM		\; \
+new-window -n TERM 'todo.sh ls; zsh'		\; \
+split -t 0 -h -l 84 htop			\; \
+split -t 1 -v -l 58 '~/.i3/colorscheme.sh; zsh'	\; \
 select-pane -t 0
 "
