a94f0a759a248f8e148106c4a6360e3b50603bc1
Moez Bouhlel <bmoez.j@gmail.com>
2016-01-05 16:13:00 +0100

    update linux config to 4.3 and enable 3G cle support update squid and conky config

--- ~/.i3/TERM.sh
+++ ~/.i3/TERM.sh
@@ -3,5 +3,5 @@ tmux kill-session -t TERM
 exec tmux new-session -nTERM -sTERM			\; \
 	new-window -n TERM 'todo.sh ls; zsh'		\; \
 	split -t 0 -h -l 84 htop			\; \
-	split -t 1 -v -l 58 'archey3; zsh'	\; \
+	split -t 1 -v -l 58 'archey3; zsh'		\; \
 	select-pane -t 0
