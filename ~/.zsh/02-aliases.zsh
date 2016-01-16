c10860fb839d4baff082d103002f5ed1ba7f8880
Moez Bouhlel <bmoez.j@gmail.com>
2016-01-16 15:19:25 +0100

    update linux config to v4.4 delete unused cunstomize.d files add more mount points on schroot for better pulseaudio, sudo, pts and cups support add alias for package management for schroots

--- ~/.zsh/02-aliases.zsh
+++ ~/.zsh/02-aliases.zsh
@@ -96,6 +96,13 @@ alias qemu='qemu-system-x86_64 -enable-kvm -cpu host -m 1G'
 alias ipt='sudo /sbin/iptables'
 alias iptlist='sudo /sbin/iptables -n -v --line-numbers -L'
 
+#Schroot
+alias schroot64="schroot -c Arch64"
+alias pacman32="sudo pacman --root /opt/arch32 --config /opt/arch32/etc/pacman.conf --dbpath /opt/arch32/var/lib/pacman/"
+alias pacman64="sudo pacman --root /opt/arch64"
+alias yaourt32="yaourt --root /opt/arch32 --config /opt/arch32/etc/pacman.conf --dbpath /opt/arch32/var/lib/pacman/"
+alias yaourt64="yaourt --root /opt/arch64"
+
 
 # Parenting changing perms on / #
 alias chown='chown --preserve-root'
