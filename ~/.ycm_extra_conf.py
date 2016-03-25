5c7de4b9d11a6d9e26a9eb0011ded1fb692a12c3
Moez Bouhlel <bmoez.j@gmail.com>
2016-03-25 22:28:27 +0100

    add YouCompleteMe extra conf file add ebooks support to vifm

---
+++ ~/.ycm_extra_conf.py
@@ -0,0 +1,61 @@
+import os
+
+flags = [
+    '-Wall',
+    '-Wextra',
+    # '-Werror',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-fexceptions',
+    '-DNDEBUG',
+    '-I',
+    '.',
+]
+
+SOURCE_EXTENSIONS = ['.cpp', '.cxx', '.cc', '.c', '.m', '.mm']
+
+def FlagsForFile(filename, **kwargs):
+    global flags
+
+    ext = os.path.splitext(filename)[1]
+    if ext in ['.cpp', '.cxx', '.cc', '.mm']:
+        flags += [
+            '-std=c++14',
+            '-x',
+            'c++',
+        ]
+    else:
+        flags += [
+            '-std=c11',
+            '-x',
+            'c',
+            '-pthread',
+            '-I/usr/include/gtk-3.0',
+            '-I/usr/include/at-spi2-atk/2.0',
+            '-I/usr/include/at-spi-2.0',
+            '-I/usr/include/dbus-1.0',
+            '-I/usr/lib/dbus-1.0/include',
+            '-I/usr/include/gtk-3.0',
+            '-I/usr/include/gio-unix-2.0/',
+            '-I/usr/include/cairo',
+            '-I/usr/include/pango-1.0',
+            '-I/usr/include/atk-1.0',
+            '-I/usr/include/cairo',
+            '-I/usr/include/pixman-1',
+            '-I/usr/include/freetype2',
+            '-I/usr/include/libpng16',
+            '-I/usr/include/harfbuzz',
+            '-I/usr/include/freetype2',
+            '-I/usr/include/harfbuzz',
+            '-I/usr/include/libdrm',
+            '-I/usr/include/libpng16',
+            '-I/usr/include/gdk-pixbuf-2.0',
+            '-I/usr/include/libpng16',
+            '-I/usr/include/glib-2.0',
+            '-I/usr/lib/glib-2.0/include',
+        ]
+
+    return {
+        'flags': flags,
+        'do_cache': True
+    }
