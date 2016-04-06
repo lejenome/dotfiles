68a8c61358742189a3e3b078e6a213cb61dbbf9f
Moez Bouhlel <bmoez.j@gmail.com>
2016-04-06 10:34:15 +0100

    fix rzsh_path.sh

--- ~/.zsh/rzsh_path.sh
+++ ~/.zsh/rzsh_path.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/zsh
+#!/usr/bin/env zsh
 echo $PWD| sed "s,^/,%F{240}, ; s,^%F{240}home/\w*,%F{240}~, ; s,/,%F{2} » %F{240},g"
 #i=1
 #echo $PWD| sed 's/^\//%F{240}/'| sed 's/^%F{240}home\/\w*/%F{240}~/'| sed -e 's/\//%F{'$i'} \\u00BB %F{240}/g'
