7f329e1afc7bb778e8887ba16726268b369e55eb
Moez Bouhlel <bmoez.j@gmail.com>
2016-07-20 17:20:53 +0100

    update ipython config to ipython-5.x

--- ~/.ipython/profile_default/ipython_config.py
+++ ~/.ipython/profile_default/ipython_config.py
@@ -3,14 +3,26 @@ c = get_config()
 c.TerminalInteractiveShell.autocall = 1
 c.TerminalInteractiveShell.automagic = True
 c.TerminalInteractiveShell.confirm_exit = False
-c.TerminalInteractiveShell.banner1 = ''
-# c.TerminalInteractiveShell.readline_parse_and_bind
+c.TerminalInteractiveShell.display_completions = 'multicolumn'
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.enable_html_pager = True
+c.TerminalInteractiveShell.highlight_matching_brackets = True
+c.TerminalInteractiveShell.sphinxify_docstring = True
 
-c.PromptManager.in_template = r'\u@ \# >> '
-c.PromptManager.in_template = r'{color.LightGreen}\u@\h{color.LightBlue}[{color.LightCyan}\Y1{color.LightBlue}]{color.Green}|\#> '
-c.PromptManager.in2_template = r'{color.Green}|{color.LightGreen}\D{color.Green}> '
-c.PromptManager.out_template = r'<\#> '
+c.TerminalInteractiveShell.mouse_support = True
+
+# c.TerminalInteractiveShell.prompts_class.prompt_in1 = r'\u@ \# >> '
+# c.TerminalInteractiveShell.prompts_class.prompt_in1 = r'{color.LightGreen}\u@\h{color.LightBlue}[{color.LightCyan}\Y1{color.LightBlue}]{color.Green}|\#> '
+# c.TerminalInteractiveShell.prompts_class.prompt_in2 = r'{color.Green}|{color.LightGreen}\D{color.Green}> '
+# c.TerminalInteractiveShell.prompts_class.prompt_out = r'<\#> '
 c.HistoryManager.db_cache_size = 50
+
+# c.TerminalInteractiveShell.banner1 = ''
+# c.TerminalInteractiveShell.banner2 = ''
+c.TerminalIPythonApp.display_banner = False
+
+c.TerminalInteractiveShell.display_page = True
+c.TerminalInteractiveShell.highlighting_style = 'native'
 
 c.InteractiveShellApp.exec_lines = ["""
 %rehashx
