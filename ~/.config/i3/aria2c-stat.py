4d2df24329503713c66244732782f0a5673543ad
Moez Bouhlel <bmoez.j@gmail.com>
2016-08-09 13:35:42 +0100

    update qutebrowser & conky config

--- ~/.config/i3/aria2c-stat.py
+++ ~/.config/i3/aria2c-stat.py
@@ -4,15 +4,25 @@ from xmlrpc.client import ServerProxy
 try:
     with ServerProxy('http://127.0.0.1:6800/rpc') as proxy:
         stat = proxy.aria2.getGlobalStat()
+        perc = ''
+        if int(stat['numActive']) > 0:
+            actives = proxy.aria2.tellActive(
+                ['completedLength', 'totalLength'])
+            cl = 0
+            tl = 0
+            for it in actives:
+                cl = cl + int(it['completedLength'])
+                tl = tl + int(it['totalLength'])
+            perc = ' ({}%)'.format(int((cl / tl) * 100))
         print("""
         {{"full_text":"{}","color":"#949494","separator":false,"separator_block_width":1}},
-        {{"full_text":"/","color":"#949494","separator":false,"separator_block_width":1}},
-        {{"full_text":"{}","color":"#909737","separator":false,"separator_block_width":1}},
+        {{"full_text":"/","color":"#949494","separator":false,"separator_block_width":2}},
+        {{"full_text":"{}{}","color":"#909737","separator":false,"separator_block_width":2}},
         {{"full_text":"/","color":"#949494","separator":false,"separator_block_width":1}},
         {{"full_text":"{}","color":"#b27d12","separator":false,"separator_block_width":6}},
         """
               .format(stat['numStopped'],
-                      stat['numActive'], stat['numWaiting']))
+                      stat['numActive'], perc, stat['numWaiting']))
 except:
     print('{"full_text":"N","color":"#924020","separator":false,'
           '"separator_block_width":6},')
--- ~/.config/i3/getNetwokStrength.sh
+++ ~/.config/i3/getNetwokStrength.sh
@@ -1,6 +1,12 @@
-[ -n "$(iw dev wlp8s0 station dump)" ]  \
-	&& (echo 100 $(iw dev wlp8s0 station dump | grep signal: | sed 's/^.*\[\(.*\)].*$/\1/' ) | bc) \
-	|| (networkctl status | head -1 | grep routable > /dev/null && echo 100) \
+#!/bin/bash
+if [ -n "$(iw dev wlp8s0 station dump)" ]; then
+	echo 100 $(iw dev wlp8s0 station dump | grep signal: | sed 's/^.*\[\(.*\)].*$/\1/' ) | bc
+	exit 0
+elif [ -n "$(iw dev wlp8s1 station dump)" ]; then
+	echo 100 $(iw dev wlp8s1 station dump | grep signal: | sed 's/^.*\[\(.*\)].*$/\1/' ) | bc
+	exit 0
+fi
+(networkctl status | head -1 | grep routable > /dev/null && echo 100) \
 	|| echo 0
 #connmanctl services $(connmanctl services | grep "^*" -m 1 | sed 's/^.* //') | awk  '$1 == "Strength" { print $3 }'
 #dbus-send --system --print-reply --dest=org.ofono $(dbus-send --system --print-reply --dest=org.ofono / org.ofono.Manager.GetModems | grep "object path" | sed 's/.*"\(.*\)"/\1/') org.ofono.NetworkRegistration.GetProperties | grep "byte.*" -o | awk '{print $2}'
