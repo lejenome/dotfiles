9b910b35c242e1c0606b98ee1d8c268518d24d07
Moez Bouhlel <bmoez.j@gmail.com>
2016-09-26 23:48:19 +0100

    update qutebrowser jupyter X11

---
+++ ~/.config/jupyter/jupyter_qtconsole_config.py
@@ -0,0 +1,17 @@
+c = get_config()
+
+c.JupyterApp.answer_yes = False
+
+c.JupyterQtConsoleApp.display_banner = False
+
+# The font family to use for the console. On OSX this defaults to Monaco, on
+# Windows the default is Consolas with fallback of Courier, and on other
+# platforms the default is Monospace.
+c.ConsoleWidget.font_family = 'Source Code Pro'
+
+
+# The type of underlying text widget to use. Valid values are 'plain', which
+# specifies a QPlainTextEdit, and 'rich', which specifies a QTextEdit.
+c.ConsoleWidget.kind = 'rich'
+
+c.JupyterWidget.editor = 'gvim --nofork {filename}'
--- ~/.config/jupyter/profile_default/ipython_config.py
+++ ~/.config/jupyter/profile_default/ipython_config.py
@@ -1,3 +1,6 @@
+from IPython.terminal.prompts import Prompts, Token
+import os
+
 c = get_config()
 
 # c.TerminalInteractiveShell.autocall = 1
@@ -5,27 +8,32 @@ c.TerminalInteractiveShell.automagic = True
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.display_completions = 'multicolumn'
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.InteractiveShell.editor = 'vim'
 c.TerminalInteractiveShell.enable_html_pager = True
 c.TerminalInteractiveShell.highlight_matching_brackets = True
 c.TerminalInteractiveShell.sphinxify_docstring = True
 
 c.TerminalInteractiveShell.mouse_support = True
 
-# c.TerminalInteractiveShell.prompts_class.prompt_in1 = r'\u@ \# >> '
-# c.TerminalInteractiveShell.prompts_class.prompt_in1 = r'{color.LightGreen}\u@\h{color.LightBlue}[{color.LightCyan}\Y1{color.LightBlue}]{color.Green}|\#> '
-# c.TerminalInteractiveShell.prompts_class.prompt_in2 = r'{color.Green}|{color.LightGreen}\D{color.Green}> '
-# c.TerminalInteractiveShell.prompts_class.prompt_out = r'<\#> '
+
 c.HistoryManager.db_cache_size = 50
 
-# c.TerminalInteractiveShell.banner1 = ''
-# c.TerminalInteractiveShell.banner2 = ''
 c.TerminalIPythonApp.display_banner = False
 
 c.TerminalInteractiveShell.display_page = True
 c.TerminalInteractiveShell.highlighting_style = 'native'
 
-c.InteractiveShellApp.exec_lines = ["""
-%rehashx
-import sys
-import os
-"""]
+class ShellPrompt(Prompts):
+
+    def in_prompt_tokens(self, cli=None):
+        return [(Token, os.getcwd()),
+                (Token.Prompt, '|'),
+                (Token.PromptNum, str(self.shell.execution_count)),
+                (Token.Prompt, ' > ')]
+c.TerminalInteractiveShell.prompts_class = ShellPrompt
+
+c.InteractiveShellApp.exec_lines = [
+    "%rehashx",
+    "import sys",
+    "import os",
+]
